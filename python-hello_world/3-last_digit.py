b9cd00fc86c40e5469177e7b6e396b8f2aaa99f5
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-11-27 12:08:08 +0100

    upload

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,6 +9,6 @@ print("Last digit of {} is {} and is ".format(number, last_digit), end="")
 if last_digit == 0 and number != 0:
     print("0")
 elif number < 0:
-    print("less than 6 and not 0")
+    print("{} and is less than 6 and not 0".format(-last_digit))
 else:
     print("greater than 5")

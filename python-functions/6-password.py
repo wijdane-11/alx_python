446d43ac58099cbfb46a06d58a4ea270ae4326ab
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-11-27 12:55:25 +0100

    upload

---
+++ python-functions/6-password.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def validate_password(password):
+    # Check the length
+    if len(password) < 8:
+        return False
+
+    # Check for at least one uppercase letter, one lowercase letter, and one digit
+    has_upper = False
+    has_lower = False
+    has_digit = False
+
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+
+    # Check for spaces
+    if ' ' in password:
+        return False
+
+    # Return True if all checks pass
+    return has_upper and has_lower and has_digit

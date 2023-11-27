404676616cc8110936d2591d5ae0e7dffa8d1959
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-11-27 11:49:38 +0100

    upload

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,6 +6,9 @@ last_digit = abs(number) % 10
 
 print("Last digit of {} is {} and is ".format(number, last_digit), end="")
 
+if number < 0:
+    last_digit *= -1  # Correct the sign for negative numbers
+
 if last_digit > 5:
     print("greater than 5")
 elif last_digit == 0:

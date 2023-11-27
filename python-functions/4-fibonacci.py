2ea074cf958e65a7b8e7b5e007ce145f3a780f37
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-11-27 13:02:34 +0100

    upload

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def fibonacci_sequence(n):
+    # Check for non-positive values
+    if n <= 0:
+        return []
+
+    # Initialize the list with the first two Fibonacci numbers
+    sequence = [0, 1]
+
+    # Generate the Fibonacci sequence up to the nth number
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    return sequence

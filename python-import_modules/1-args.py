d681947c313c0a1bd85689af764d34c840f9ba82
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-04 12:03:40 +0100

    upload

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+    plural_suffix = 's' if num_args != 1 else ''
+
+    print("{} argument{}:".format(num_args, plural_suffix) + (':' if num_args else '.'))
+
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))

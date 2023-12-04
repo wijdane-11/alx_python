955bfecea1e4c579ad03fc025ab2913afbf2633c
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-04 16:07:29 +0100

    upload

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,24 @@
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+
+if __name__ == "__main__":
+    def print_sorted_dictionary(my_dict):
+        """ Print sorted dictionary """
+        keys = sorted(my_dict.keys())
+        for k in keys:
+            print("{}: {}".format(k, my_dict[k]))
+
+    a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+    new_dict = update_dictionary(a_dictionary, 'language', "Python")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)
+
+    print("--")
+    print("--")
+
+    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)

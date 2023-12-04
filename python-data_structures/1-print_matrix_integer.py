6f5e9cc391228a89b3ece6d0f1d8f6cdc2b55cd9
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-04 15:40:23 +0100

    upload

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,16 @@
+def print_matrix_integer(matrix=[]):
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()

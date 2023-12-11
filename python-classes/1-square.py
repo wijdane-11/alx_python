d0829229f443e568c41fb0c60b03bb7b123e6909
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-11 11:33:48 +0100

    upload

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,10 @@
-def __init__(self, size=0):
-    """Initialize the Square instance"""
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    self.__size = size
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """Initialize the Square instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

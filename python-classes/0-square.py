25071967ef51c8eb694b216c313bd253c694998a
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-11 10:49:50 +0100

    upload

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 class Square:
-    """Square class"""
+    """Define a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
 
     def __init__(self, size):
         """Initialize the Square instance with a given size.

4af4ab5bbac8d1a4ec73035bd70625c2c3ad47d5
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-11 12:44:26 +0100

    upload

---
+++ python-classes/3-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""3-square module"""
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """Initialize the Square instance"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with type and value validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Compute the area of the square"""
+        return self.__size * self.__size
+
+if __name__ == "__main__":
+    my_square = Square(89)
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    my_square.size = 3
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    try:
+        my_square.size = "5 feet"
+        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    except Exception as e:
+        print(e)

d183e9c04faec72112b3ee28d2bb7065495042a8
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-11 16:16:56 +0100

    upload

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     Returns:
         True if obj is an instance of a class that inherited from a_class, else False.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class

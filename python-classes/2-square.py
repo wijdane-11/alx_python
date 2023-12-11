9b2370de7855084f4e5fd1df1a87667a6f5a0db3
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-11 12:31:15 +0100

    upload

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+class Square:
+    def __init__(self, size=0):
+        """Initialize the Square instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size ** 2

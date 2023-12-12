7c6735c496e0338bd0cfbae8c2e4b65a9ae4a507
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-12 14:41:54 +0100

    upload

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,12 @@
 def inherits_from(obj, a_class):
     """
-    Returns True if the object is an instance of a class
-    that inherited (directly or indirectly) from the specified class;
-    otherwise, returns False.
+    Check if an object is an instance of a class that inherited from a specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The specified class.
+
+    Returns:
+        True if obj is an instance of a class that inherited from a_class; otherwise, False.
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) != a_class

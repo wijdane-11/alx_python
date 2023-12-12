1f855e7afd5050d51706720a0391f74299c05ec9
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-12 14:57:35 +0100

    upload

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+Module for inherits_from function.
+"""
+
 def inherits_from(obj, a_class):
     """
     Check if the object is an instance of a class that inherited from the specified class.

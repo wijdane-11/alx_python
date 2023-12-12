1988a736e582d423a03233fc2b21bbb9e7c1ea2d
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-12 15:48:00 +0100

    upload

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+Module 4-base_geometry
+Defines the BaseGeometry class with an area method.
+"""
+
 class BaseGeometry:
+    """
+    A class representing a base geometry.
+
+    Methods:
+    - area(self): Raises an Exception with the message "area() is not implemented".
+    """
     def area(self):
+        """
+        Calculate the area. This method is not implemented in the base class.
+        """
         raise Exception("area() is not implemented")
 
 if __name__ == "__main__":

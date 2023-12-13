fd805d3b1e57a011c383369d6ea439f45f079977
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 15:54:00 +0100

    upload

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,52 @@
 #!/usr/bin/python3
-"""Module documentation"""
-class Square:
-    """Square class"""
+"""Defines a Square class"""
 
-    def __ini__(self, size=0):
-        """Initialize the Square instance"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes the square"""
         self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Calculates the area of the square"""
+        return self.__size ** 2
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print(type(my_square_1))
+    print(my_square_1.__dict__)
+
+    my_square_2 = Square()
+    print(type(my_square_2))
+    print(my_square_2.__dict__)
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_3 = Square("3")
+        print(type(my_square_3))
+        print(my_square_3.__dict__)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_4 = Square(-89)
+        print(type(my_square_4))
+        print(my_square_4.__dict__)
+    except Exception as e:
+        print(e)

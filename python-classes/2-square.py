9a80f69d42c45ae02a9d82215d36652627d6dd9f
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 15:56:39 +0100

    upload

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -24,8 +24,7 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+          raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size

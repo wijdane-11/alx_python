aa0680d5730af8f4bc9c6735a5d69b825b8d15f1
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 12:18:33 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++
@@ -1,9 +0,0 @@
-class Meta(type):
-    def __repr__(self):
-        return "<3-base_geometry.BaseGeometry object at 0x...>"
-
-BaseGeometry = Meta('BaseGeometry', (), {})
-
-bg = BaseGeometry()
-
-print(bg)

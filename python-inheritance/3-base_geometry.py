1bf1924020208fbfbe05fa23e09e0ffa80b2d7da
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 12:53:27 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,7 @@
 class BaseGeometry:
-    def __repr__(self):
-        return '<3-base_geometry.BaseGeometry object at 0x...>'
+    pass
 
 bg = BaseGeometry()
-print(bg)
+
+print(dir(bg))
+print(dir(BaseGeometry))

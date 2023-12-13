160d79762bb2b6a7d6c08b7ff25b408f2be1bbf9
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 13:34:43 +0100

    upload

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle class that inherits from BaseGeometry."""
+
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """A class representing a Rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle instance."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Return the representation of the Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+if __name__ == "__main__":
+    r = Rectangle(3, 5)
+    print(r)
+    print(dir(r))
+
+    try:
+        print("Rectangle: {} - {}".format(r.width, r.height))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))

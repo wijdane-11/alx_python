95866513a5ffa093bb80ff09134023c05f7ef753
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 15:47:13 +0100

    upload

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,31 +1,36 @@
-class BaseGeometry:
-    def area(self):
-        raise NotImplementedError("area() is not implemented")
+#!/usr/bin/python3
+"""Module that defines a Square class"""
 
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+Rectangle = __import__('7-rectangle').Rectangle
 
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
 
-    def area(self):
-        return self.__width * self.__height
+def integer_validator(name, value):
+    """Validates if the value is an integer greater than 0."""
+    if type(value) is not int:
+        raise TypeError(f"{name} must be an integer")
+    if value <= 0:
+        raise ValueError(f"{name} must be > 0")
+
 
 class Square(Rectangle):
-    def __init__(self, size):
-        self.__size = size
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+    """Square class that inherits from Rectangle"""
 
-    def __str__(self):
-        return f"[Square] {self.__size}/{self.__size}"
+    def __init__(self, size):
+        integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def area(self):
-        return self.__size ** 2
+        """Compute the area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return a string representation of the square"""
+        return f"[Square] {self.__size}/{self.__size}"
+
+
+if __name__ == "__main__":
+    s = Square(13)
+
+    print(s)
+    print(s.area())

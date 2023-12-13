3eec6b7ea4960aef4ff5c7f587cb5c60ed11889f
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 12:32:34 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,19 +1,11 @@
-Write an empty class BaseGeometry.
+class Meta(type):
+    def __repr__(self):
+        return "<3-base_geometry.BaseGeometry object at 0x...>"
 
-    You are not allowed to import any module
+    __doc__ = "This is the BaseGeometry module."
 
-guillaume@ubuntu:~/$ cat 3-main.py
-#!/usr/bin/python3
-BaseGeometry = __import__('3-base_geometry').BaseGeometry
+BaseGeometry = Meta('BaseGeometry', (), {})
 
 bg = BaseGeometry()
 
 print(bg)
-print(dir(bg))
-print(dir(BaseGeometry))
-
-guillaume@ubuntu:~/$ python3 3-main.py
-<3-base_geometry.BaseGeometry object at 0x7f2050c69208>
-['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
-['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
-guillaume@ubuntu:~/$ 

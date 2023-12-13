14d133a1e2991669d50acc0474aa2c6bbe57983d
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 12:33:28 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,14 @@
 class Meta(type):
-    def __repr__(self):
-        return "<3-base_geometry.BaseGeometry object at 0x...>"
-
-    __doc__ = "This is the BaseGeometry module."
+    def __dir__(self):
+        return [
+            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__',
+            '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__',
+            '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+            '__subclasshook__', '__weakref__'
+        ]
 
 BaseGeometry = Meta('BaseGeometry', (), {})
 
 bg = BaseGeometry()
 
-print(bg)
+print(dir(bg))

20dd24b48e3b7a970c230aee0d173ef01942a1c6
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 13:33:05 +0100

    upload

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,8 +1,24 @@
+"""This module defines the BaseGeometry class."""
+
 class BaseGeometry:
+    """A class representing basic geometry operations."""
+
     def area(self):
+        """Raises an Exception indicating that area() is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Validates an integer value.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:

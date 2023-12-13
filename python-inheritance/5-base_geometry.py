aae1ed198a31bce3bc41a8572fd5cd67b77c6439
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 09:46:08 +0100

    upload

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -8,23 +8,24 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
-# Test cases
-bg = BaseGeometry()
+# Test case
+if __name__ == "__main__":
+    bg = BaseGeometry()
+    print(dir(bg))
 
-bg.integer_validator("my_int", 12)
-bg.integer_validator("width", 89)
+    try:
+        bg.integer_validator("my_int", 12)
+        bg.integer_validator("width", 89)
+        bg.integer_validator("name", "John")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    bg.integer_validator("name", "John")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    try:
+        bg.integer_validator("age", 0)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    bg.integer_validator("age", 0)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("distance", -4)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    try:
+        bg.integer_validator("distance", -4)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))

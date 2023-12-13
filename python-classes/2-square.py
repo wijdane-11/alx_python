9e5acb99543529e027191875afc72be2e00d759c
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 15:56:04 +0100

    upload

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -20,7 +20,7 @@ class Square:
         Args:
             size (int): The size of the square. Default is 0.
 
-        Raises:
+        
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """

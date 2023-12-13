187c7fc6b26ea8c51b85b88b6ab17b0d430e3f39
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 12:50:21 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,35 +1,6 @@
-Correct output - case: bg = BaseGeometry() print(dir(bg))
+class BaseGeometry:
+    def __repr__(self):
+        return '<3-base_geometry.BaseGeometry object at 0x...>'
 
-    main_1.py
-
-     - [Got]
-    <3-base_geometry.BaseGeometry object at 0x7f3cc8ab2280>
-    ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
-
-    (401 chars long)
-    [stderr]: 
-    (0 chars long)
-    [Expected]
-    ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
-
-    (324 chars long)
-    [stderr]: [Anything]
-    (0 chars long)
-
-Correct output - case: print(dir(BaseGeometry))
-
-    main_2.py
-
-     - [Got]
-    <3-base_geometry.BaseGeometry object at 0x7f50b6ee6280>
-    ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
-
-    (401 chars long)
-    [stderr]: 
-    (0 chars long)
-    [Expected]
-    ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
-
-    (324 chars long)
-    [stderr]: [Anything]
-    (0 chars long)
+bg = BaseGeometry()
+print(bg)

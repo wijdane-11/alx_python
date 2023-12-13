024b34f1026a09d2138fbe45fba4b077c6c38f86
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 14:00:37 +0100

    upload

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,7 +1,5 @@
-"""Module for geometric shapes."""
-
 class BaseGeometry:
-    """A class for basic geometry operations."""
+    """A class for basic geometric operations."""
 
     def area(self):
         """Raise an exception since the area calculation is not implemented."""
@@ -22,26 +20,3 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
-class Rectangle(BaseGeometry):
-    """A class representing a rectangle."""
-
-    def __init__(self, width, height):
-        """Initialize the rectangle with given width and height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Calculate the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
-        return self.__width * self.__height

ba10c27d06fe013555739fb14ed4a16ff1521d31
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 11:40:36 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,15 +1,8 @@
-#!/usr/bin/python3
-"""
-This module defines an empty class BaseGeometry.
-"""
-
 class BaseGeometry:
-    """
-    An empty class representing the base geometry.
-    """
     pass
 
 bg = BaseGeometry()
+
 print(bg)
 print(dir(bg))
 print(dir(BaseGeometry))

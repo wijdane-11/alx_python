588a2f86abcdf5a884c17db427eb4bb7c1d2a76d
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 15:40:45 +0100

    upload

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,31 +1,41 @@
+#!/usr/bin/python3
 class BaseGeometry:
-    """A class for basic geometric operations.
-
-    Attributes:
-        name (str): The name of the value.
-        value (int): The value to validate.
-
-    Methods:
-        area(): Raises an exception since the area calculation is not implemented.
-        integer_validator(name, value): Validates if the value is an integer and greater than 0.
-    """
-
     def area(self):
-        """Raise an exception since the area calculation is not implemented."""
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate if the value is an integer and greater than 0.
-
-        Args:
-            name (str): The name of the value.
-            value (int): The value to validate.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
+
+
+if __name__ == "__main__":
+    s = Square(13)
+
+    print(s)
+    print(s.area())

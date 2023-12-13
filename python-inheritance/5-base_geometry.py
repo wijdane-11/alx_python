fe5d779071e9f42179d928fbb62871b35eb01d79
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 13:14:49 +0100

    upload

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,9 @@
+"""Module 5-base_geometry
+
+This module contains the definition of the BaseGeometry class.
+
+"""
+
 class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")

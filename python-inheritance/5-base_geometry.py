4bbf35548281bbf73bd53b8aafe21b29a09c59b2
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-13 13:14:00 +0100

    upload

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,28 +4,6 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-# Test case
-if __name__ == "__main__":
-    bg = BaseGeometry()
-    print(dir(bg))
-
-    try:
-        bg.integer_validator("my_int", 12)
-        bg.integer_validator("width", 89)
-        bg.integer_validator("name", "John")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("age", 0)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("distance", -4)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+            raise ValueError("{} must be greater than 0".format(name))

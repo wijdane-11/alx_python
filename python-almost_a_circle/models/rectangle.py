44f2f9dbd22b7ec8fc278e52dc0d5d1e8c0e8975
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-18 12:09:43 +0100

    upload

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Module defining the Rectangle class."""
+from models.base import Base
+
+class Rectangle(Base):
+    """The Rectangle class, inherits from Base."""
+
+    def _init_(self, width, height, x=0, y=0, id=None):
+        """Initialize the Rectangle instance.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int): X-coordinate of the rectangle.
+            y (int): Y-coordinate of the rectangle.
+            id (int): If provided, assign it to the public instance attribute id.
+                      Otherwise, let the super class handle it.
+        """
+        super()._init_(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y."""
+        self.__y = value

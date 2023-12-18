47173f87dac41d5368bec6c056ebe068930ef9fd
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-18 11:21:16 +0100

    upload

--- python-almost_a_circle/models/models/base.py
+++ python-almost_a_circle/models/models/base.py
@@ -1,11 +1,22 @@
+#!/usr/bin/python3
+"""Module defining the Base class."""
+
 class Base:
+    """The Base class for managing id attribute."""
+
+    # Private class attribute
     __nb_objects = 0
 
-    def __init__(self, id=None):
+    # Class constructor
+    def _init_(self, id=None):
+        """Initialize the Base instance.
+
+        Args:
+            id (int): If provided, assign it to the public instance attribute id.
+                      Otherwise, increment __nb_objects and assign the new value to id.
+        """
         if id is not None:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
-
-# noqa: Module not documented or not enough - <module 'models.base' from '...'>
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

3ca1ec1328cac8c783d4c3774139b7b70dff4c64
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-18 11:19:14 +0100

    upload

--- python-almost_a_circle/models/models/base.py
+++ python-almost_a_circle/models/models/base.py
@@ -1,25 +1,28 @@
+# models/base.py
+
 class Base:
-    __nb_objects = 0  # private class attribute
+    """
+    Base class for managing unique IDs for instances.
+    
+    Private Class Attribute:
+        __nb_objects (int): Counter for objects, initialized to 0.
+
+    Public Methods:
+        __init__(self, id=None): Constructor method for Base class.
+    """
+
+    __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initializes an instance of the Base class with a unique ID.
+
+        Args:
+            id (int, optional): ID to assign to the instance. If not provided,
+                                increments the counter and assigns the new value.
+        """
         if id is not None:
-            self.id = id  # assign id if provided
+            self.id = id
         else:
-            type(self).__nb_objects += 1  # increment __nb_objects for each new instance
-            self.id = type(self).__nb_objects  # assign incremented value as id
-
-if __name__ == "__main__":
-    b1 = Base()
-    print(b1.id)  # Output: 1
-
-    b2 = Base()
-    print(b2.id)  # Output: 2
-
-    b3 = Base()
-    print(b3.id)  # Output: 3
-
-    b4 = Base(12)
-    print(b4.id)  # Output: 12
-
-    b5 = Base()
-    print(b5.id)  # Output: 4
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects

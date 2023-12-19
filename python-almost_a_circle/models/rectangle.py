33d48829f61b141e971e60910221b0b22c53b000
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-19 15:53:45 +0100

    upload

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ models/rectangle.py """
 
-from models.base import Base
+class Base:
+    def __init__(self, id):
+        self.id = id
 
 class Rectangle(Base):
     """A class representing a rectangle."""

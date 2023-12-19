9ca28b578c02322251161a3211fc9eacb8fc3a2d
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-19 15:51:56 +0100

    upload

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,19 @@
 from models.base import Base
 
 class Rectangle(Base):
+    """A class representing a rectangle."""
+    
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle object.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): X coordinate of the rectangle. Defaults to 0.
+            y (int, optional): Y coordinate of the rectangle. Defaults to 0.
+            id (int, optional): ID of the rectangle. Defaults to None.
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -13,32 +25,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Getter for the width attribute."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter for the width attribute."""
         self.__width = value
 
     @property
     def height(self):
+        """Getter for the height attribute."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter for the height attribute."""
         self.__height = value
 
     @property
     def x(self):
+        """Getter for the x attribute."""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Setter for the x attribute."""
         self.__x = value
 
     @property
     def y(self):
+        """Getter for the y attribute."""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Setter for the y attribute."""
         self.__y = value

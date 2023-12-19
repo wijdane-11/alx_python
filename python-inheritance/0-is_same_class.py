6de35e173730b82881bea563fbbf97b714481816
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-19 15:56:53 +0100

    upload

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -14,4 +14,4 @@ def is_same_class(obj, a_class):
     Returns:
         bool: True if obj is exactly an instance of a_class, otherwise False.
     """
-    return type(obj) is a_class
+    return type(obj) is a_class ,

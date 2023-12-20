c8c2c3f527dfdcb2f63b7d76c4621446c27ea1d8
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2023-12-20 11:33:54 +0100

    upload

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,26 +1,8 @@
-# models/square.py
-from models.rectangle import Rectangle
+from models.rectangle import Rectangle  # Assuming Rectangle class exists
 
 class Square(Rectangle):
-    """A class representing a square."""
-
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square object.
-
-        Args:
-            size (int): The size of the square.
-            x (int, optional): X coordinate of the square. Defaults to 0.
-            y (int, optional): Y coordinate of the square. Defaults to 0.
-            id (int, optional): ID of the square. Defaults to None.
-        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """
-        Overrides the __str__ method to provide a custom string representation.
-
-        Returns:
-            str: String representation of the square.
-        """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
